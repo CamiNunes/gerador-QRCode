7cf1f40ea4bab0e3b61083b46b4e8d143591abae
CamilaNunes <cvn.camila@gmail.com>
2022-08-17 20:47:05 -0300

    Criando o QRCode;

--- gerador-qrcode/src/App.js
+++ gerador-qrcode/src/App.js
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
+
 import './App.css';
 
 function App() {
+  const [link, setLink] = useState('');
+  const [qrCodeLink, setQrCodeLink] = useState('');
+
+  function handleGenerateImg(link_url){
+    QRCodeLink.toDataURL(link_url, {
+      width: 600,
+      margin: 3
+    }, function (err, url){
+      setQrCodeLink(url);
+    });
+  }
+
+  function handleQrcode(event){
+    setLink(event.target.value);
+    handleGenerateImg(event.target.value);
+  }
+
   return (
     <div className="container">
-     <h1>Gerador de QRCode</h1>
-     <input className="input" placeholder="Digite seu link..."/>
+      <h1>Gerador de QRCode</h1>
+
+      <QRCode value={link}/>
+
+
+      <input className="input" placeholder="Digite seu link..." value={link} onChange={ (event) => handleQrcode(event)}/>
+
+      <a href={qrCodeLink} download={`qrcode.png`}>Baixar QRCode</a>
+      
     </div>
   );
 }
